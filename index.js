9196e59dda73efd091936003f221481d2648fa3b
LightLord <stevensah88@gmail.com>
2021-10-16 02:42:29 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("App Started")
+});
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
+})
